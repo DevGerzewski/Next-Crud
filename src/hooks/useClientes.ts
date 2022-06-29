255f71e901f53c68ce2882d4de7c43d8db93d9ab
DevGerzewski <carlos-gerzewski@hotmail.com>
2022-06-28 22:00:45 -0300

    Crud funcionando integrado com firebase

---
+++ src/hooks/useClientes.ts
@@ -0,0 +1,60 @@
+import { useEffect, useState } from "react"
+import ColecaoCliente from "../backend/db/ColecaoCliente"
+import Cliente from "../core/Cliente"
+import ClienteRepositorio from "../core/ClienteRepositorio"
+import useTabelaOuForm from "./useTabelaOuForm"
+
+export default function useClientes(){
+  const repo: ClienteRepositorio = new ColecaoCliente()
+
+  const {
+    tabelaVisivel,
+    exibirformulario,
+    exibirTabela
+  } = useTabelaOuForm()
+
+  const [cliente, setCliente] = useState<Cliente>(Cliente.vazio())
+  const [clientes, setClientes] = useState<Cliente[]>([])
+
+  useEffect(obterTodos, [])
+  
+  function obterTodos() {
+    repo.obterTodos().then(clientes => {
+      setClientes(clientes)
+      exibirTabela()
+    }) 
+  }
+
+  function selecionarCliente(cliente: Cliente){
+    setCliente(cliente)
+    exibirformulario()
+  }
+  
+  async function excluirCliente(cliente: Cliente){
+    await repo.excluir(cliente)
+    obterTodos()
+  }
+
+  function novoCliente(){
+    setCliente(Cliente.vazio())
+    exibirformulario()
+  }
+
+  async function salvarCliente(cliente: Cliente){
+    await repo.salvar(cliente)
+    obterTodos()
+  }
+
+  return{
+    cliente,
+    clientes,
+    novoCliente,
+    salvarCliente,
+    excluirCliente,
+    selecionarCliente,
+    obterTodos,
+    tabelaVisivel,
+    exibirTabela
+  }
+
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,21 @@
+import Botao from "../components/Botao";
+import Formulario from "../components/Formulario";
 import Layout from "../components/Layout";
 import Tabela from "../components/Tabela";
-import Cliente from "../core/Cliente";
+import useClientes from "../hooks/useClientes";
 
 export default function Home() {
 
-  const clientes = [
-    new Cliente('Ana', 34, '1'),
-    new Cliente('Bia', 24, '2'),
-    new Cliente('Anderson', 44, '3'),
-    new Cliente('José', 54, '4')
-  ]
+  const {
+    cliente,
+    clientes,
+    novoCliente,
+    salvarCliente,
+    selecionarCliente,
+    excluirCliente,
+    tabelaVisivel,
+    exibirTabela
+  } = useClientes()
 
   return (
     <div className={`
@@ -18,8 +24,29 @@ export default function Home() {
       text-white
     `}>
       <Layout titulo="Cadastro Simples">
-        <Tabela clientes={clientes}></Tabela>
+        {tabelaVisivel ? (
+          <>
+            <div className="flex justify-end">
+              <Botao 
+                cor="green" 
+                className="mb-4"
+                onClick={novoCliente}>
+                  Novo Cliente
+              </Botao>
+            </div>
+            <Tabela clientes={clientes} 
+            clienteSelecionado={selecionarCliente}
+            clienteExcluido={excluirCliente}
+            />
+          </>
+        ): (
+          <Formulario 
+            cliente={cliente}
+            clienteMudou={salvarCliente}
+            cancelado={exibirTabela}
+          />
+        )}
       </Layout>
     </div>
   )
-}
+}  
